9a94d8a35b2a10f9a5301405cc08fd79682ff307
fuyu <yu1.fu@jince.com>
2018-03-21 11:22:04 +0800

    提交代码

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://yindupharmacy.com/admin/');
+define('HTTP_CATALOG', 'http://yindupharmacy.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://yindupharmacy.com/admin/');
+define('HTTPS_CATALOG', 'http://yindupharmacy.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/yindupharmacy/public_html/admin/');
+define('DIR_SYSTEM', '/home/yindupharmacy/public_html/system/');
+define('DIR_LANGUAGE', '/home/yindupharmacy/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/yindupharmacy/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/yindupharmacy/public_html/system/config/');
+define('DIR_IMAGE', '/home/yindupharmacy/public_html/image/');
+define('DIR_CACHE', '/home/yindupharmacy/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/yindupharmacy/public_html/system/download/');
+define('DIR_UPLOAD', '/home/yindupharmacy/public_html/system/upload/');
+define('DIR_LOGS', '/home/yindupharmacy/public_html/system/logs/');
+define('DIR_MODIFICATION', '/home/yindupharmacy/public_html/system/modification/');
+define('DIR_CATALOG', '/home/yindupharmacy/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'yindupha_yindu');
+define('DB_PASSWORD', 'Cc$$$@@@###zzz');
+define('DB_DATABASE', 'yindupha_yindu');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://yindupharmacy.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://yindupharmacy.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/yindupharmacy/public_html/catalog/');
+define('DIR_SYSTEM', '/home/yindupharmacy/public_html/system/');
+define('DIR_LANGUAGE', '/home/yindupharmacy/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/yindupharmacy/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/yindupharmacy/public_html/system/config/');
+define('DIR_IMAGE', '/home/yindupharmacy/public_html/image/');
+define('DIR_CACHE', '/home/yindupharmacy/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/yindupharmacy/public_html/system/download/');
+define('DIR_UPLOAD', '/home/yindupharmacy/public_html/system/upload/');
+define('DIR_MODIFICATION', '/home/yindupharmacy/public_html/system/modification/');
+define('DIR_LOGS', '/home/yindupharmacy/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'yindupha_yindu');
+define('DB_PASSWORD', 'Cc$$$@@@###zzz');
+define('DB_DATABASE', 'yindupha_yindu');
+define('DB_PREFIX', 'oc_');
